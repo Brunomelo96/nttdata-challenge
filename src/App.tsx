be7e587d283dc92e9e4f7850b9912b29d82380a7
Bruno <brunobeggar@gmail.com>
2023-02-20 11:09:29 -0300

    feat: creating Header, Footer components and adding Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
+import "./App.module.scss"
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <div>
-      empty
-    </div>
+    <>
+      <Home />
+    </>
   )
 }
 
